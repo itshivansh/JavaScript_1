e69e72d419de75870e21be4414576f7e7e241e79
shivansh <shivanshakb@gmail.com>
2020-09-24 07:35:04 +0000

    session4

---
+++ Session_4/ASPProjectSimple/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="ASPProjectSimple.Global_asax" Language="vb" %>
---
+++ Session_4/ConsoleFrameworkApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using FrameworkLibraryApp;
+using System;
+using System.Windows.Forms;
+
+namespace ConsoleFrameworkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.employeeId = 63;
+            Console.WriteLine(employee.employeeId);
+            MessageBox.Show(employee.employeeId.ToString());
+            Console.ReadLine();
+        }
+    }
+}
